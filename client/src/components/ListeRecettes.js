5ede36eb396ab7c2f63051d238737c2a3433ec5b
LucasLiaoR <lucasliao6@gmail.com>
2021-03-20 10:57:09 +0100

    Init projet

--- client/src/components/ListeRecettes.js
+++ client/src/components/ListeRecettes.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
 import '../App.css';
-
+import imgRecette1 from '../resources/r1.png'
 
 
 //Importer en dynamique les images d'un folder
@@ -11,7 +11,6 @@ import '../App.css';
   
 // const images = importAll(require.context('./', false, /\.(png|jpe?g|svg)$/));
 
-const image = require('../resources/r1.png');
 
 class RechercheRecette extends Component {
 
@@ -38,11 +37,9 @@ class RechercheRecette extends Component {
         </div>
     )
 
-
-    renderRecipes2 = ({id_recette, libelle_recette, difficulte, temps_prep, nom_fichier_image}) => 
-    (
-        <div key={id_recette} className="card" style={{width: '18rem', margin:'20px'}}>
-            <img src={`/static/images/${nom_fichier_image}`} className="card-img-top" style={{height:'200px'}} alt="..." />
+    renderRecipes2 = ({id_recette, libelle_recette, difficulte, temps_prep}) => (
+        <div className="card" style={{width: '18rem', margin:'20px'}}>
+            <img src={imgRecette1} className="card-img-top" alt="..." />
             <div className="card-body">
                 <h5 className="card-title">{libelle_recette}</h5>
                 <p className="card-text">Difficulté : {difficulte} - Temps : {temps_prep} min</p>
@@ -51,18 +48,15 @@ class RechercheRecette extends Component {
         </div>
     )
 
-    
+
+
     render(){
         const {recipes} = this.state;
         return (
-            <div>
-                <h1>Toutes nos recettes</h1>
-                <div className='sectionResultat'>
-                    {recipes.map(this.renderRecipes2)}
-                </div>
-            </div>
 
-            
+            <div className='sectionResultat'>
+                {recipes.map(this.renderRecipes2)}
+            </div>
         )
     }
 
